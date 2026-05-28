a74ef24d7705e55d7f10fcf3380b4e0ac276b852
vinp505 <pirasvincenzo05@gmail.com>
2026-05-28 12:49:38 +0200

    Commented generate_plot.py code.

--- streamlit_setup/generate_plot.py
+++ streamlit_setup/generate_plot.py
@@ -67,22 +67,23 @@ def generate_week_lineplot(df_list, color_list, seed):
         # store filtered dataset 
         df_week_list.append(df_week)
     
-
+    # iterate through axes
     for i, ax in enumerate(axes):
-
+        
+        # axis settings
         ax.axis('off')
         ax.set_ylim(close_min - 1, close_max + 1)
-        #ax.set_xlim(-2, len(df_week))
-        #ax.vlines(x= len(df_week)/2, ymin= -100, ymax= 100, colors= 'black')
+
+        # obtain correct dataset
         df_week = df_week_list[i]
 
-        # ROUND VALS TO INTEGERS
-        #ax.text(x= len(df_week)/2, y = close_max + abs(close_max*0.2), s= f"Stock {i+1}", c= color_list[i%4], ha= 'center', weight= 550, size= 50)
+        # round 40% and 80% of min and max values to integers and add the relative line
         ax.hlines(y = round(close_min*0.8, 0)/2, xmin= -1, xmax = len(df_week), linewidth= 3, colors= '#C8C8C7', clip_on=False)
         ax.hlines(y = round(close_min*0.8, 0), xmin= -1, xmax = len(df_week), linewidth= 3, colors= '#C8C8C7')
         ax.hlines(y = round(close_max*0.8, 0)/2, xmin= -1, xmax = len(df_week), linewidth= 3, colors= '#C8C8C7')
         ax.hlines(y = round(close_max*0.8, 0), xmin= -1, xmax = len(df_week), linewidth= 3, colors= '#C8C8C7')
-        #ax.vlines(x = len(df_week)/2, ymin=-100, ymax= 100, colors= 'black')
+        
+        # add labels with % in between plots (if lines aren't too close to eachother)
         if i < 3:
             if close_min*0.4 < -0.8 or (close_max - close_min) < 15:
                 ax.text(x=len(df_week)+2.3, y=round(close_min*0.8, 0) - abs(close_min)*0.01, s=f"{(close_min*0.8):.0f}%", c= '#C8C8C7', ha='center', weight= 550, size= 30)
@@ -90,24 +91,35 @@ def generate_week_lineplot(df_list, color_list, seed):
             if close_max*0.4 > 0.8 or (close_max - close_min) < 15:
                 ax.text(x=len(df_week)+2.3, y=round(close_max*0.8, 0) - abs(close_max)*0.01, s=f"{'+' * (float(close_max) > 0)}{(close_max*0.8):.0f}%", c= '#C8C8C7', ha='center', weight= 550, size= 30)
             ax.text(x= len(df_week)+2.3, y= -0.1, s= '0%', c= 'black', ha='center', weight= 550, size= 30)
-            
+
+        # add 0 line     
         ax.hlines(y= 0, xmin= -1, xmax= len(df_week), linewidth= 5, colors= 'black')
+
+        # scatter for end points + arrow on 0 line
         ax.scatter([0, 13], [0, df_week['week close (%)'].values[-1]], c= color_list[i%4], s= 300, zorder= 5)
         ax.scatter([14], [0], c= '#000000', s= 300, marker= '>', zorder= 4)
+
+        # main data plotting
         ax.plot(range(len(df_week)), df_week['week close (%)'], c= color_list[i], linewidth= 10)
 
+    # obtain dates of time period
     date_1, date_2 = df_week['report_date'].values[0], df_week['report_date'].values[-1]
+    
     return fig, stock_min_final, stock_max_final, stock_min_peak, stock_max_peak, date_1, date_2
 
+# -------------------------------------------------------------------------------
 
 def generate_final_lineplot(df_list, color_list, name_list, x_ticks, x_labels):
 
+    # axes with proper ratio (Apple goes off scale)
     fig, axes = plt.subplots(4, 1, gridspec_kw={'height_ratios': [1.85, 1, 1, 1]}, facecolor= '#FAF9F6')
     fig.set_figheight(32)
     fig.set_figwidth(20)
 
+    # iterate through axes
     for i, ax in enumerate(axes):
         
+        # text for the major horizontal lines
         ax.text(x= 211, y= 170, s= f'Stock {i+1}: ', c= 'black', ha= 'right', size= 'large', weight= 550)
         ax.text(x= 215, y= 170, s= name_list[i], c= color_list[i], ha= 'left', size= 'x-large', weight= 'heavy')
         ax.text(x=-15, y=5, s="0%", c= 'black', ha='center', weight= 550, size= 8) #, ha='center', family='serif', fontname='Arial')
@@ -116,6 +128,9 @@ def generate_final_lineplot(df_list, color_list, name_list, x_ticks, x_labels):
         ax.text(x=-15, y=155, s="+150%", c= '#C8C8C7', ha='center', weight= 550, size= 8)
         ax.text(x=-15, y=-45, s=" -50%", c= '#C8C8C7', ha='center', weight= 550, size= 8)
         ax.text(x=-15, y=-95, s="-100%", c= '#C8C8C7', ha='center', weight= 550, size= 8)
+
+
+        # horizontal lines for major and minor ticks
 
         ax.hlines(y=0, xmin=0, xmax=1759, colors='black')
         ax.hlines(y=25, xmin=0, xmax=1759, colors='#E1E1DD', linewidth= 0.8)
@@ -131,6 +146,9 @@ def generate_final_lineplot(df_list, color_list, name_list, x_ticks, x_labels):
         ax.hlines(y=-100, xmin=0, xmax=1759, colors='#C8C8C7', linewidth= 1.3)
         ax.hlines(y=-120, xmin=-2, xmax=1759, colors='black')
 
+
+        # dashed lines for the margins of the graph
+
         ax.hlines(y=0, xmin=-100, xmax=0, colors='black', linestyles= 'dashed')
         ax.hlines(y=25, xmin=-100, xmax=0, colors='#E1E1DD', linestyles= 'dashed', linewidth= 0.8)
         ax.hlines(y=50, xmin=-100, xmax=0, colors='#C8C8C7', linestyles= 'dashed', linewidth= 1.3)
@@ -145,7 +163,6 @@ def generate_final_lineplot(df_list, color_list, name_list, x_ticks, x_labels):
         ax.hlines(y=-100, xmin=-100, xmax=0, colors='#C8C8C7', linestyles= 'dashed', linewidth= 1.3)
         ax.hlines(y=-120, xmin=-100, xmax=0, colors='black', linestyles= 'dashed')
         
-
         ax.hlines(y=0, xmin=1759, xmax=1900, colors='black', linestyles= 'dashed')
         ax.hlines(y=25, xmin=1759, xmax=1900, colors='#E1E1DD', linestyles= 'dashed', linewidth= 0.8)
         ax.hlines(y=50, xmin=1759, xmax=1900, colors='#C8C8C7', linestyles= 'dashed', linewidth= 1.3)
@@ -161,7 +178,7 @@ def generate_final_lineplot(df_list, color_list, name_list, x_ticks, x_labels):
         ax.hlines(y=-120, xmin=1759, xmax=1900, colors='black', linestyles= 'dashed')
         
 
-        
+        # set custom axis parameters for Apple, extra line
         if df_list[i]['symbol'].values[0] == 'AAPL':
             ax.set_ylim(-150, 500)
             ax.hlines(y=400, xmin=-100, xmax=0, colors='#C8C8C7', linestyles= 'dashed', linewidth= 1.3)
@@ -172,11 +189,15 @@ def generate_final_lineplot(df_list, color_list, name_list, x_ticks, x_labels):
         else:
             ax.set_ylim(-150, 200)
         
+        # plot values, adjust axis
         ax.plot(df_list[i]['report_date'], df_list[i]['close (%)'], c= color_list[i], linewidth= 2.5)
         ax.axis('off')
         ax.set_xlim(left= -50, right= 1809)
         
+        # add x-axis labels and ticks
         for x, label in zip(x_ticks, x_labels):
+            
+            # plot years lower than months
             if len(label) > 2:
                 size = 'medium'
                 ymin = -137
@@ -185,6 +206,8 @@ def generate_final_lineplot(df_list, color_list, name_list, x_ticks, x_labels):
                 size = 'small'
                 ymin = -125
                 y = -127
+            
+            # add vertical line for the ticks, and text for the labels
             ax.vlines(x=x, ymin=ymin, ymax=-120, colors='black')
             ax.text(
                 x=x, 
@@ -195,6 +218,7 @@ def generate_final_lineplot(df_list, color_list, name_list, x_ticks, x_labels):
     
     return fig
 
+# -------------------------------------------------------------------------------
 
 def generate_heatmap(sector_mom):
 
@@ -263,27 +287,35 @@ def generate_heatmap(sector_mom):
 
     return fig
 
+# -------------------------------------------------------------------------------
+
 def generate_barplot(df, avg_close, ticks_2019: bool = False):
 
+    # obtain baseline values in $
     baseline_sector_avg = {item[0][1] : item[1] for item in avg_close['average close'].items()}
+    
+    # more data
     quarters = set([k[0] for k in df['average close'].keys()])
     sectors = [k[1] for k in df['average close'].keys()]
     values = df['average close'].values
     unique_sectors = sorted(baseline_sector_avg.keys())
     unique_sectors = unique_sectors[:8] + ['Real Estate', 'Technology', 'Utilities']
     
+    # main plot definition
     fig, axes = plt.subplots(11, 1, figsize= (24, 80), facecolor= '#FAF9F6')
     fig.subplots_adjust(
         top=0.98, 
         bottom=0.02, 
-        hspace=0.08  # Adjust this to control the vertical spacing *between* the 11 subplots
+        hspace=0.08
     )
 
+    # for each sector
     for i, sec in enumerate(unique_sectors):
 
         ax = axes[i]
         ax.set_ylim(-60, 60)
 
+        # horizontal tick lines
         ax.hlines(y=10, xmin=-0.7, xmax=27.7, colors='#E1E1DD', linewidth= 0.8, zorder= 0)
         ax.hlines(y=20, xmin=-0.7, xmax=27.7, colors='#C8C8C7', linewidth= 1.3, zorder= 0)
         ax.hlines(y=30, xmin=-0.7, xmax=27.7, colors='#E1E1DD', linewidth= 0.8, zorder= 0)
@@ -294,6 +326,9 @@ def generate_barplot(df, avg_close, ticks_2019: bool = False):
         ax.hlines(y=-30, xmin=-0.7, xmax=27.7, colors='#E1E1DD', linewidth= 0.8, zorder= 0)
         ax.hlines(y=-40, xmin=-0.7, xmax=27.7, colors='#C8C8C7', linewidth= 1.3, zorder= 0)
 
+
+        # relative text (mirrored, both at left and right margins)
+        
         ax.text(x= -1.4, y=1, s="0%", c= 'black', ha='left', weight= 550)
         ax.text(x= -1.8, y=19.2, s="+20%", c= '#C8C8C7', ha='left', weight= 550)
         ax.text(x= -1.8, y=39.2, s="+40%", c= '#C8C8C7', ha='left', weight= 550)
@@ -305,14 +340,21 @@ def generate_barplot(df, avg_close, ticks_2019: bool = False):
         ax.text(x= 28.8, y=-20.8, s="-20%", c= '#C8C8C7', ha='right', weight= 550)
         ax.text(x= 28.8, y=-40.8, s="-40%", c= '#C8C8C7', ha='right', weight= 550)
 
+        # obtain sector values
         sec_values = [values[i] for i in range(len(sectors)) if sectors[i] == sec]
+
+        # convert sector values in % change from sector baseline
         sec_values_lday_perc = [((values[i] - baseline_sector_avg[sec]) / baseline_sector_avg[sec]) * 100 for i in range(len(sectors)) if sectors[i] == sec]
+        
+        # convert sector values in % change from previous quarter value
         diff_Q = [0] + [((sec_values[i] - sec_values[i-1]) / sec_values[i-1]) * 100 for i in range(1, len(sec_values))]
         
-
         colors = ["#4C98CE" if diff_Q[j] > 0 else "#D06A4C" for j in range(len(diff_Q))]
         
+        # plot quarterly change bars
         bars2 = ax.bar(range(len(diff_Q)), diff_Q, color= colors)
+        
+        # plot horizontal lines for change from baseline if needed
         for i, val in enumerate(sec_values_lday_perc):
             q = str(i%4+1)
 
@@ -326,7 +368,9 @@ def generate_barplot(df, avg_close, ticks_2019: bool = False):
                 ha='center',
                 va='top', size= 'medium', weight= 550, zorder= 4)
 
+        # x ticks and labels: quarter number, and overarching year
         for x, yr in [(1, '2019'), (5, '2020'), (9, '2021'), (13, '2022'), (17, '2023'), (21, '2024'), (25, '2025')]:
+            
             ax.hlines(y=-51.7, xmin= x-1.05, xmax= x+2.05, colors= 'black', linewidth= 1.6, zorder= 4)
 
             ax.text(
@@ -336,14 +380,17 @@ def generate_barplot(df, avg_close, ticks_2019: bool = False):
                 ha='center',
                 va='top', size= 'large', weight= 550, zorder= 4)
 
+        # 0 line and x-axis line
         ax.hlines(y=0, xmin=-1.3, xmax=28.3, colors='black', linewidth= 1.6, zorder= 4)
         ax.hlines(y=-47, xmin=-1.3, xmax=28.3, colors='black', linewidth= 1.6, zorder= 4)
+        
+        # add sector name
         ax.text(x=0, y=+34, s=sec, c= 'black', ha='left', size= 'x-large', weight= 'heavy')
         ax.axis('off')
         
     return fig
 
-
+# -------------------------------------------------------------------------------
 
 def plot_spiral(vol_pct, price_pct):
     import pandas as pd
@@ -707,10 +754,10 @@ def plot_spiral(vol_pct, price_pct):
         
         # Update text values next to thresholds
         text_min.set_position((-1, hist_min))
-        text_min.set_text(f"Min\n{hist_min:+.1f}%\n{df.loc[hist_min_idx, "report_date"].strftime("%d/%m/%Y")}")
+        text_min.set_text(f"""Min\n{hist_min:+.1f}%\n{df.loc[hist_min_idx, 'report_date'].strftime("%d/%m/%Y")}""")
         
         text_max.set_position((-1, hist_max))
-        text_max.set_text(f"Max\n{hist_max:+.1f}%\n{df.loc[hist_max_idx, "report_date"].strftime("%d/%m/%Y")}")
+        text_max.set_text(f"""Max\n{hist_max:+.1f}%\n{df.loc[hist_max_idx, 'report_date'].strftime("%d/%m/%Y")}""")
         
         # Toggle year markers
         for ann in annotation_objects:
